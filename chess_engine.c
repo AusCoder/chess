6b161c1b3b8f41e715754dfccf35184af3dfab9a
Joshua Seaton <josh.a.seaton@gmail.com>
2016-05-04 14:30:27 -0400

    initial commit of chess_engine

---
+++ chess_engine.c
@@ -0,0 +1,16 @@
+
+int state = 1; //1 if game is in play; 0 if checkmate or resignation occurs
+int turn = 0; //0 if white's turn; 1 if black's
+
+int main()
+{
+	while(state == 1){
+		// interpret user input;
+		// update hashmap;
+		// redraw GUI;	
+		turn = !turn; /*changes turns*/	
+	
+	}
+	// display winner	
+	return 0
+}
